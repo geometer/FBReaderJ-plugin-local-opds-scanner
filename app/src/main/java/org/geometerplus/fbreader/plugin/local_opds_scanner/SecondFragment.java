c0cf289fd9df9b89efe0edcb97cec8493683b465
slesh <slesh.eugene93@gmail.com>
2020-10-13 22:43:36 +0300

    Draft version of main activity with simple navigation buttons(wip)

---
+++ app/src/main/java/org/geometerplus/fbreader/plugin/local_opds_scanner/SecondFragment.java
@@ -0,0 +1,34 @@
+package org.geometerplus.fbreader.plugin.local_opds_scanner;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+}
