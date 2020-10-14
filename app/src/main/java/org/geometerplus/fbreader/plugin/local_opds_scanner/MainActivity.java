a3efb409ad7ec6b77ffdcee6770862de3a0caa11
slesh <slesh.eugene93@gmail.com>
2020-10-14 18:42:16 +0300

    Removed auto-generated useless UI elements

--- app/src/main/java/org/geometerplus/fbreader/plugin/local_opds_scanner/MainActivity.java
+++ app/src/main/java/org/geometerplus/fbreader/plugin/local_opds_scanner/MainActivity.java
@@ -2,36 +2,21 @@ package org.geometerplus.fbreader.plugin.local_opds_scanner;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void openScanLocalNetworkActivity(View view) {
         startActivity(new Intent(this, ScanLocalNetworkActivity.class));
     }
 
-    public void openLocanIpActivity(View view) {
+    public void openLocalIpActivity(View view) {
         startActivity(new Intent(this, LocalIPActivity.class));
     }
 }
